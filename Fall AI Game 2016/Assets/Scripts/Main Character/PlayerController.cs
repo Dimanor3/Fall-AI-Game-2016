b1561f4d4b5aef17f89c122cd7d9534a5b7df4f7
Dimanor3 <bijanrazavi15@yahoo.com>
2016-08-27 02:25:25 -0400

    Added player movement (player can now walk, run, use stamina if running and regen stamina so long as they aren't running)

---
+++ Fall AI Game 2016/Assets/Scripts/Main Character/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerMotor))]
+[RequireComponent(typeof(Stamina))]
+public class PlayerController : MonoBehaviour {
+
+	// Player movement
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private float runSpeed;
+	private PlayerMotor motor;					// Used to move the player
+
+	// Player stamina
+	private Stamina stamina;
+	[SerializeField] private float playerStamina;
+	[SerializeField] private float useStaminaSpeed;
+	[SerializeField] private float staminaRegen;
+
+	// Use this for initialization
+	void Start () {
+		// Initialize access to all outside classes
+		motor = GetComponent<PlayerMotor> ();
+		stamina = GetComponent<Stamina> ();
+
+		// Initialize all required variables
+		moveSpeed = 5f;
+		runSpeed = 10f;
+		playerStamina = 1000f;
+		useStaminaSpeed = 5f;
+		staminaRegen = 2f;
+
+		// Initialize stamina properties
+		stamina.setStamina(playerStamina);
+		stamina.setStaminaLoss (useStaminaSpeed);
+		stamina.setStaminaRegen (staminaRegen);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Running?
+		float run = Input.GetAxis("Run");
+
+		// Main Character left right up and down movement
+		float horizontalMovement = Input.GetAxis("Horizontal");
+		float verticalMovement = Input.GetAxis("Vertical");
+
+		// Calculations for main characters movements
+		Vector2 moveHorizontal = transform.right * horizontalMovement;
+		Vector2 moveVertical = transform.up * verticalMovement;
+		Vector2 movement = (moveHorizontal + moveVertical).normalized;
+
+		// Set player's movement speed
+		movement *= running(run);
+
+		// Use the player's stamina?
+		useStamina (run, horizontalMovement, verticalMovement);
+
+		// Regen the player's stamina?
+		regenStamina (run, horizontalMovement, verticalMovement);
+
+		// Move the player
+		motor.SetMovement (movement);
+	}
+
+	// Determines whether or not the player is running or walking
+	// and outputs the speed accordingly
+	float running(float run){
+		if (run != 0 && stamina.getStamina () > 0) {
+			return runSpeed;
+		} else {
+			return moveSpeed;
+		}
+	}
+
+	// Checks to see if stamina is being used and uses it if it is
+	void useStamina(float run, float hM, float vM){
+		if (run != 0 && stamina.getStamina () > 0 && (hM != 0 || vM != 0)) {
+			stamina.useStamina ();
+		}
+	}
+
+	// Checks to see whether or not the player can regen their stamina
+	void regenStamina(float run, float hM, float vM){
+		if (run == 0 && hM == 0 && vM == 0){
+			stamina.regen ();
+		}
+
+		if (run == 0 && (hM != 0 || vM != 0)){
+			stamina.halfRegen ();
+		}
+	}
+}
---
+++ Fall AI Game 2016/Assets/Scripts/Main Character/Stamina.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stamina : MonoBehaviour {
+
+	// Player's stamina
+	[SerializeField] private float stamina = 0f;
+	private float maxStamina = 0f;
+	[SerializeField] private float staminaLoss = 0f;		// Stamina lost per frame
+	[SerializeField] private float staminaRegen = 0f;	// Stamina regained per frame
+	
+	// Update is called once per frame
+	void Update () {
+		if(stamina >= maxStamina){
+			stamina = maxStamina;
+		}
+
+		if (stamina <= 0f) {
+			stamina = 0f;
+		}
+	}
+
+	// Regenerate the players stamina over time
+	public void regen(){
+		stamina += staminaRegen;
+	}
+
+	// Regenerate the players stamin at half the speed
+	public void halfRegen(){
+		stamina += staminaRegen / 2;
+	}
+
+	// Set the amount of stamina the player has
+	public void setStamina(float s){
+		stamina = maxStamina = s;
+	}
+
+	// Set the players stamina regen speed
+	public void setStaminaRegen(float r){
+		staminaRegen = r;
+	}
+
+	// Use stamina
+	public void useStamina(){
+		stamina -= staminaLoss;
+	}
+
+	// Set the rate at which the player loses stamina
+	public void setStaminaLoss(float sL){
+		staminaLoss = sL;
+	}
+
+	// Get players stamina
+	public float getStamina(){
+		return stamina;
+	}
+}
