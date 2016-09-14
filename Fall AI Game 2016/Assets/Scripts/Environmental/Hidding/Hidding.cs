e4405489b760f284a57010e30443d89735dc83a1
Bijan Razavi <brazavi@uncc.edu>
2016-09-14 13:01:28 -0400

    Changed and deleted somethings. Added hidding spots, press e near one to use.

---
+++ Fall AI Game 2016/Assets/Scripts/Environmental/Hidding/Hidding.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hidding : MonoBehaviour {
+
+	// Detects if the player used the use button
+	[SerializeField] private bool used = false;
+
+	// Checked to see if the user is already hidding or not
+	[SerializeField] private bool hidding = false;
+
+	// Check to see if the player can enter the hidding spot
+	[SerializeField] private bool enter = false;
+	
+	// Update is called once per frame
+	void Update () {
+		float use = Input.GetAxis ("Use");
+
+		// Makes sure the user can't spam use
+		if (use != 0 && !used && enter) {
+			if (!hidding) {
+				hidding = true;
+			} else {
+				hidding = false;
+			}
+
+			used = true;
+		}
+
+		// Makes it so that the user can use use again
+		// after they've finished using it once
+		if (use == 0) {
+			used = false;
+		}
+	}
+
+	// Used to tell the guard if the player is, or isn't,
+	// in the line of sight
+	public bool getHidding(){
+		return hidding;
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.CompareTag("Hidding Spot")) {
+			enter = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D col) {
+		if (col.CompareTag("Hidding Spot")) {
+			enter = false;
+		}
+	}
+}
--- Fall AI Game 2016/Assets/Scripts/Main Character/PlayerMotor.cs
+++ Fall AI Game 2016/Assets/Scripts/Main Character/PlayerMotor.cs
@@ -8,10 +8,13 @@ public class PlayerMotor : MonoBehaviour {
 
 	// Movement stuff
 	private Vector2 movement;		// Move the player
+	private Vector2 aiMovement;		// Move the player via AI
 
 	// Rotation stuff
 	private Vector2 rotation;		// Rotate the player
 	private float rotationSpeed;	// Rotation speed
+
+	private bool hidden = false; // Is the player hidden?
 
 	// Use this for initialization
 	void Start () {
@@ -19,32 +22,49 @@ public class PlayerMotor : MonoBehaviour {
 		rb = GetComponent<Rigidbody2D> ();
 
 		// Player movement initialization
-		movement = new Vector2(0f,0f);
+		movement = new Vector2 (0f,0f) ;
 
 		// Player rotation initialization
-		rotation = new Vector2(0f,0f);
+		rotation = new Vector2 (0f,0f) ;
 		rotationSpeed = 0f;
 	}
 	
 	// Run every physics iteration
 	void FixedUpdate () {
-		movePlayer ();
+		if (hidden) {
+			kinematicMove ();
+		} else {
+			movePlayer ();
+		}
 	}
 
 	// Set the players movement
-	public void SetMovement(Vector2 move){
+	public void SetMovement (Vector2 move) {
 		movement = move;
 	}
 
 	// Set the players rotation
-	public void SetRotationSpeed(float rS){
+	public void SetRotationSpeed (float rS) {
 		rotationSpeed = rS;
 	}
 
+	public void SetAIMovement (Vector2 move) {
+		aiMovement = move;
+	}
+
+	public void SetHidden (bool hide) {
+		hidden = hide;
+	}
+
 	// Move the player
-	void movePlayer(){
-		if(movement != Vector2.zero){
+	void movePlayer () {
+		if (movement != Vector2.zero) {
 			rb.MovePosition (rb.position + movement * Time.fixedDeltaTime);
 		}
+	}
+
+	// Move player via AI
+	void kinematicMove () {
+		rb.velocity = aiMovement;
 	}
 }
