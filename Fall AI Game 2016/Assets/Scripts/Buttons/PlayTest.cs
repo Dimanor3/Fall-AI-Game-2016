65683918e2f7f4bb5f0be2979ff64d088e71c00b
Bijan Razavi <brazavi@uncc.edu>
2016-09-15 00:19:49 -0400

    Added a basic main menu.

---
+++ Fall AI Game 2016/Assets/Scripts/Buttons/PlayTest.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayTest : MonoBehaviour {
+	public void PlayTestButton () {
+		SceneManager.LoadScene(2);
+	}
+}
