c0749f4bc619c8e00d3800d2942bbc1abce033f7
Jeremy Abel <jabel415@gmail.com>
2016-12-01 17:44:45 -0500

    Added guard movement through patrol points

---
+++ Fall AI Game 2016/Assets/Scripts/Guard/Patrol.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrol : MonoBehaviour {
+	
+	public Transform[] points;
+	private int destPoint = 0;
+	private NavMeshAgent agent;
+
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.autoBraking = false;
+
+		GoToNextPoint ();
+	}
+
+	void GoToNextPoint() {
+		if (points.Length == 0)
+			return;
+		agent.destination = points [destPoint].position;
+
+		destPoint = (destPoint + 1) % points.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (agent.remainingDistance < 0.5f)
+			GoToNextPoint();
+	}
+}
